8aa38edbdddf0ba34e948fb9c758fcef8feed306
hazimazmi <hazimazmi229@gmail.com>
2025-06-10 02:40:58 +0800

    title : FitPlex

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Web App</title>
+        <title>FitPlex</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

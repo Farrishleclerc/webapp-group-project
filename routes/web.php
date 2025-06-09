a7e3f407dd3351b0b82de6611610c894567f0602
hazimazmi <hazimazmi229@gmail.com>
2025-06-10 02:57:18 +0800

    mainpage

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/mainpage', function () {
+    return view('mainpage');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });

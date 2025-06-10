69aecc1923e629520ff042a696ba25f85f27f4d6
Farrishleclerc <farrishazmi1620@icloud.com>
2025-06-10 19:19:10 +0800

    latest

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,22 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
-
-
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('mainpage');
-});
+})->name('home');
 
-// Add login route
-Route::get('/login', function () {
-    return view('login'); // Assuming your login blade is in resources/views/login.blade.php
-})->name('login');
+// Login routes
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
-Route::get('/register', function () {
-    return view('register'); // Assuming your login blade is in resources/views/login.blade.php
-})->name('register');
+// Registration routes
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
 
+// Contact route
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
-
-// Booking Routes
-Route::prefix('booking')->group(function () {
-    Route::get('/single', [BookingController::class, 'single'])->name('booking.single');
-    Route::get('/package', [BookingController::class, 'package'])->name('booking.package');
-});
